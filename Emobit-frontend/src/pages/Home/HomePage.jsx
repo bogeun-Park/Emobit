06c380f4d8d5a83fc8ba32037f9821cac2e547e9
pbg <qhrms0528@softbase.co.kr>
2025-04-16 17:44:38 +0900

    디폴트 페이지 추가

---
+++ Emobit-frontend/src/pages/Home/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function HomePage() {
+    return (
+        <div>
+            <h2>홈입니다!</h2>
+        </div>
+    );
+}
+
+export default HomePage;
