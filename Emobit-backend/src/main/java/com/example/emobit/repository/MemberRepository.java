017eb878ceed3bde23b2c68a578bce480fccbd1d
pbg <qhrms0528@softbase.co.kr>
2025-04-16 17:33:07 +0900

    Member테이블의 repository 생성

---
+++ Emobit-backend/src/main/java/com/example/emobit/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.example.emobit.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.emobit.domain.Member;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long>{
+	Optional<Member> findByUsername(String username);  // Derived query methods
+}
