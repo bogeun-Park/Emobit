31f9d909073a465b07054e1a311ac7910dd78971
pbg <qhrms0528@softbase.co.kr>
2025-04-16 17:41:49 +0900

    Layout, Routes 렌더링 구성 수정

--- Emobit-frontend/src/App.js
+++ Emobit-frontend/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import routes from "./routes";
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <div className="App">
-      Hello, Emobit!
-    </div>
+    <Router>
+      <Layout>
+        <Routes>
+          {routes.map((route, idx) => (
+            <Route key={idx} path={route.path} element={route.element} />
+          ))}
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
