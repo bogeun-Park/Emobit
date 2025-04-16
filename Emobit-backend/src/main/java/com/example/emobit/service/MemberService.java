c15a4d21de6bae9eec0f7b5b7ad0cfaa51d1507a
pbg <qhrms0528@softbase.co.kr>
2025-04-16 17:33:51 +0900

    Member테이블의 service생성, 회원을 DB에 저장하는 api추가

---
+++ Emobit-backend/src/main/java/com/example/emobit/service/MemberService.java
@@ -0,0 +1,34 @@
+package com.example.emobit.service;
+
+import java.util.Map;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.emobit.domain.Member;
+import com.example.emobit.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+	private final MemberRepository memberRepository;
+	private final PasswordEncoder passwordEncoder;
+	
+	public void saveMember(Map<String, String> formData) {
+        String displayName = formData.get("displayName");
+        String username = formData.get("username");
+        String password = formData.get("password");
+        
+        // 비밀번호 암호화
+        String encodedPassword = passwordEncoder.encode(password);
+
+        Member member = new Member();
+        member.setDisplayName(displayName);
+        member.setUsername(username);
+        member.setPassword(encodedPassword);
+
+        memberRepository.save(member);
+	}
+}
