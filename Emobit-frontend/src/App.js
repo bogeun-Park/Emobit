b36270cfbd3c20650ba619c91603fac80a7902cf
bogeun-Park <qhrms0528@gmail.com>
2025-04-16 20:36:23 +0900

    안쓰는 import 삭제

--- Emobit-frontend/src/App.js
+++ Emobit-frontend/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from "react-router-dom";
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import routes from "./routes";
 import Layout from './components/Layout';
 
