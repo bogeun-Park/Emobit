0b375b8d5b1bddbba8056144b211b68915b5577b
pbg <qhrms0528@softbase.co.kr>
2025-04-17 17:33:29 +0900

    현재 로그인 정보를 나타내는 버튼 추가(임시)

--- Emobit-frontend/src/pages/Home/HomePage.jsx
+++ Emobit-frontend/src/pages/Home/HomePage.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function HomePage() {
+    const handleGetUser = () => {
+        axios.get(`${process.env.REACT_APP_API_URL}/login/auth`)
+            .then(response => {
+                console.log('User data:', response.data);
+            })
+            .catch(error => {
+                console.error('Failed to fetch user data:', error);
+            });
+    };
+
     return (
         <div>
             <h2>홈입니다!</h2>
+
+            <button onClick={handleGetUser}>Get User Info</button>
         </div>
     );
 }
