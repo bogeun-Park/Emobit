1ac4dfcf15f4be7f9379a306c2b9a7ba957fd1c2
pbg <qhrms0528@softbase.co.kr>
2025-04-17 17:59:37 +0900

    로그아웃 구현현

--- Emobit-frontend/src/components/Header.jsx
+++ Emobit-frontend/src/components/Header.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import '../styles/Header.css'
 
 function Header() {
+    const navigate = useNavigate();
+
+    function logout() {
+        axios.post(`${process.env.REACT_APP_API_URL}/logout`)
+            .then(response => {
+                console.log(response);
+                navigate('/login');  // 로그인 페이지로 이동
+            })
+            .catch(error => {
+                console.error('Failed to fetch user data:', error);
+            });
+    }
+
     return (
         <header>
             <h3><Link to="/">Emobit</Link></h3>
@@ -14,6 +29,7 @@ function Header() {
 
             <nav>
                 <Link to="/login">로그인</Link>
+                <button onClick={logout}>로그아웃</button>
             </nav>
         </header>
     );
