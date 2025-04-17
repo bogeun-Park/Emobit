1c917899e45a9ab0d8cde1866057bc4e070479ed
pbg <qhrms0528@softbase.co.kr>
2025-04-17 18:03:16 +0900

    사용자가 요청시 쿠키 인증 정보도 포함하도록 변경

--- Emobit-backend/src/main/java/com/example/emobit/config/WebConfig.java
+++ Emobit-backend/src/main/java/com/example/emobit/config/WebConfig.java
@@ -9,6 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("*");
+                .allowedMethods("*")
+        		.allowCredentials(true);
     }
 }
