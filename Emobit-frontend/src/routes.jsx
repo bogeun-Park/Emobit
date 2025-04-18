c054f0638957b445d435763f3359b6f493c0a1a0
pbg <qhrms0528@softbase.co.kr>
2025-04-18 18:05:16 +0900

    라우팅 경로 추가

--- Emobit-frontend/src/routes.jsx
+++ Emobit-frontend/src/routes.jsx
@@ -3,13 +3,17 @@ import HomePage from "./pages/Home/HomePage";
 import LoginPage from "./pages/Login/LoginPage";
 import RegisterPage from "./pages/Login/RegisterPage";
 import RegisterSucess from "./pages/Login/RegisterSucess";
+import BoardPage from "./pages/Board/BoardPage";
+import Create from "./pages/Board/Create";
 
 const routes = [
     { path: "*", element: <NotFoundPage /> },
     { path: "/", element: <HomePage /> },
     { path: "/login", element: <LoginPage /> },
     { path: "/login/register", element: <RegisterPage /> },
-    { path: "/login/register/sucess", element: <RegisterSucess /> }
+    { path: "/login/register/sucess", element: <RegisterSucess /> },
+    { path: "/board", element: <BoardPage /> },
+    { path: "/board/create", element: <Create /> }
 ];
 
 export default routes;
