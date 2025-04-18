b6f7cdb5b07db54423a7dbd18224a3343417b660
pbg <qhrms0528@softbase.co.kr>
2025-04-18 18:15:32 +0900

    Validation 검증 전용 예외처리 생성

---
+++ Emobit-backend/src/main/java/com/example/emobit/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.example.emobit.exception;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult().getAllErrors().stream()
+            .map(error -> error.getDefaultMessage())
+            .collect(Collectors.toList());
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
