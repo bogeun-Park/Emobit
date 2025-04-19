d6b5ec111c654ebbda1d9eb923aaee63012ed0b6
bogeun-Park <qhrms0528@gmail.com>
2025-04-20 00:59:08 +0900

    새로고침시 auth의 상태가 유지되도록 구현

--- Emobit-frontend/src/App.js
+++ Emobit-frontend/src/App.js
@@ -2,16 +2,19 @@ import './App.css';
 import { Routes, Route } from "react-router-dom";
 import routes from "./routes";
 import Layout from './components/Layout';
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        {routes.map((route, idx) => (
-          <Route key={idx} path={route.path} element={route.element} />
-        ))}
-      </Routes>
-    </Layout>
+    <AuthProvider>
+      <Layout>
+        <Routes>
+          {routes.map((route, idx) => (
+            <Route key={idx} path={route.path} element={route.element} />
+          ))}
+        </Routes>
+      </Layout>
+    </AuthProvider>
   );
 }
 
---
+++ Emobit-frontend/src/contexts/AuthContext.js
@@ -0,0 +1,39 @@
+import React, { createContext, useState, useEffect } from 'react';
+import { useAxios } from '../contexts/AxiosContext';
+import { useDispatch } from 'react-redux';
+import { authAction } from '../redux/Slice/authSlice';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const axios = useAxios();
+
+    const [loading, setLoading] = useState(true);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+    axios.get('/login/auth')
+        .then(response => {
+            const id = response.data.id;
+            const displayName = response.data.displayName;
+            const username = response.data.username;
+            const role = response.data.role;
+            
+            if (id){
+                dispatch(authAction.login({ username, id, displayName, role }));
+            }
+        })
+        .catch(error => {
+            console.log('인증 정보 가져오기 실패:', error);
+        })
+        .finally(() => {
+            setLoading(false);
+        });
+  }, [dispatch]);
+
+  return (
+    <AuthContext.Provider value={{ loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
