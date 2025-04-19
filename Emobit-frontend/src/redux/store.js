d6d22e273bbf2d7fd9e97667d8502b9488437bf6
bogeun-Park <qhrms0528@gmail.com>
2025-04-20 01:00:48 +0900

    redux store 생성

---
+++ Emobit-frontend/src/redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './Slice/authSlice';
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer
+  }
+});
+
+if (process.env.NODE_ENV === 'development') {
+  window.store = store;
+}
