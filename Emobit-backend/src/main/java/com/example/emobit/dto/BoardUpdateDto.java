092040d053f30af84549340d2c325db17ad2ba53
bogeun-Park <qhrms0528@gmail.com>
2025-04-20 22:10:08 +0900

    게시글 업데이트 DTO 추가

---
+++ Emobit-backend/src/main/java/com/example/emobit/dto/BoardUpdateDto.java
@@ -0,0 +1,17 @@
+package com.example.emobit.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class BoardUpdateDto {
+	@NotBlank(message = "Title name is required")
+	String title;
+	
+	@NotBlank(message = "Content name is required")
+    String content;
+}
