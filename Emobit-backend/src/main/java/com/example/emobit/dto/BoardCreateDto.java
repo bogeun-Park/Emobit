d11b0858d225b5e06ce9b2b7122f969093ba89fc
bogeun-Park <qhrms0528@gmail.com>
2025-04-20 22:09:37 +0900

    프론트에서 CreatedBy를 받지 않도록 해당 컬럼 삭제

--- Emobit-backend/src/main/java/com/example/emobit/dto/BoardCreateDto.java
+++ Emobit-backend/src/main/java/com/example/emobit/dto/BoardCreateDto.java
@@ -12,9 +12,6 @@ public class BoardCreateDto {
 	@NotBlank(message = "Title name is required")
 	String title;
 	
-	@NotBlank(message = "Conmtent name is required")
+	@NotBlank(message = "Content name is required")
     String content;
-    
-	@NotBlank(message = "CreatedBy name is required")
-    String createdBy;
 }
