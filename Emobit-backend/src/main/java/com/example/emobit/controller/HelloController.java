24d4528198c258f9a35b2ea8bac1bb8735db0b3a
bogeun-Park <qhrms0528@gmail.com>
2025-04-20 22:08:41 +0900

    필요없는 소스 제거

--- Emobit-backend/src/main/java/com/example/emobit/controller/HelloController.java
+++ Emobit-backend/src/main/java/com/example/emobit/controller/HelloController.java
@@ -2,27 +2,17 @@ package com.example.emobit.controller;
 
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class HelloController {
-	@GetMapping("/hello")
-	public Map<String, String> hello() {
-        Map<String, String> response = new HashMap<String, String>();
-        response.put("message", "react, spring 연동");
-        return response;
-    }
-	
 	@Autowired
     private DataSource dataSource;
     
