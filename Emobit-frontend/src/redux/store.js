a63a8bd0c9fef9eda1d8361f27c0529d4e3be298
bogeun-Park <qhrms0528@gmail.com>
2025-04-20 14:31:02 +0900

    store 전역변수 제거

--- Emobit-frontend/src/redux/store.js
+++ Emobit-frontend/src/redux/store.js
@@ -2,11 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './Slice/authSlice';
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer
-  }
+    reducer: {
+        auth: authReducer
+    }
 });
-
-if (process.env.NODE_ENV === 'development') {
-  window.store = store;
-}
