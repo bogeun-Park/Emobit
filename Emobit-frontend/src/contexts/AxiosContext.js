2ef997055f645c56b094bd5c8caf954074e1f033
bogeun-Park <qhrms0528@gmail.com>
2025-04-20 22:05:27 +0900

    JWT 토큰 만료 시 auth 상태 초기화를 위해 로그아웃 액션 디스패치 기능을 하는 axios 인터셉터 추가

--- Emobit-frontend/src/contexts/AxiosContext.js
+++ Emobit-frontend/src/contexts/AxiosContext.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import { createContext, useContext } from 'react';
+import { store } from '../redux/store';
+import { authAction } from '../redux/Slice/authSlice'
 
 const AxiosContext = createContext();
 
@@ -9,6 +11,18 @@ export const axiosInstance = axios.create({
 });
 
 export const AxiosProvider = ({ children }) => {
+    axiosInstance.interceptors.response.use(
+        response => response,
+        error => {
+            if (error.response && error.response.status === 401) {
+                // JWT 만료 등 인증 에러 시 auth 상태 초기화
+                store.dispatch(authAction.logout());
+            }
+    
+            return Promise.reject(error);
+        }
+    );
+
     return (
     <AxiosContext.Provider value={axiosInstance}>
         {children}
