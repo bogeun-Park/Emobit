bf5ff8f02728e67263f51ed95f0c426f110ffd10
pbg <qhrms0528@softbase.co.kr>
2025-04-25 17:50:27 +0900

    이미지 업로드 구현

--- Emobit-frontend/src/pages/Board/BoardCreate.jsx
+++ Emobit-frontend/src/pages/Board/BoardCreate.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAxios } from '../../contexts/AxiosContext';
 import { useSelector } from 'react-redux';
+import presignedUrlAxios from 'axios';
 
 function BoardCreate() {
     const axios = useAxios();
@@ -10,8 +11,9 @@ function BoardCreate() {
 
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
+    const [image, setImage] = useState(null);
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
         if (!auth.isAuthenticated) {
@@ -23,26 +25,52 @@ function BoardCreate() {
         const confirmed = window.confirm('게시글을 작성하시겠습니까?');
         if (!confirmed) return;
 
-        axios.post('/board/create_process', {
-            title: title,
-            content: content,
-        })
-            .then(res => {
-                console.log('게시글 작성 완료:', res.data);
-                alert('게시글이 성공적으로 작성되었습니다.');
-                navigate('/board');
-            })
-            .catch(error => {
-                console.error('게시글 작성 실패:', error);
-
-                // 인증 오류(401)인 경우에만 로그인 페이지로 리다이렉트
-                if (error.response && error.response.status === 401) {
-                    alert('로그인 세션이 만료되었습니다. 다시 로그인해주세요.');
-                    navigate('/login');
-                } else {
-                    alert('게시글 작성 중 오류가 발생했습니다.');
+        let imageUrl = '';
+        try {
+            if (image) {
+                let presignedUrl = '';
+                // Presigned URL 요청
+                try {
+                    const presignedRes = await axios.get(`/list/PresignedUrl?filename=${image.name}`);
+                    presignedUrl = presignedRes.data;
+                } catch (error) {
+                    console.error('Presigned URL 요청 실패:', error);
+                    return;
                 }
+
+                // Object Storage에 이미지 업로드
+                try {
+                    await presignedUrlAxios.put(presignedUrl, image, {
+                        headers: {
+                            'Content-Type': image.type
+                        },
+                    });
+
+                    imageUrl = presignedUrl.replace(/\/p\/[^/]+\//, "/");
+                } catch (error) {
+                    console.error('오브젝트 스토리지 업로드 실패:', error);
+                    return;
+                }
+            }
+
+            await axios.post('/board/create_process', {
+                title,
+                content,
+                imageUrl,
             });
+
+            alert('게시글이 성공적으로 작성되었습니다.');
+            navigate('/board');
+        } catch (error) {
+            console.error('게시글 작성 중 에러 발생:', error);
+
+            if (error.response?.status === 401) {
+                alert('로그인 세션이 만료되었습니다. 다시 로그인해주세요.');
+                navigate('/login');
+            } else {
+                alert('게시글 작성 중 오류가 발생했습니다.');
+            }
+        }
     };
 
     return (
@@ -58,6 +86,11 @@ function BoardCreate() {
                 <div style={{ display: 'flex' }}>
                     <label htmlFor="content">내용</label>
                     <textarea id="content" value={content} onChange={(e) => setContent(e.target.value)} rows={8}/>
+                </div>
+
+                <div>
+                    <label htmlFor="fileInput"></label>
+                    <input type="file" id="fileInput" onChange={(e) => setImage(e.target.files[0])} />
                 </div>
 
                 <button type="submit">저장</button>
