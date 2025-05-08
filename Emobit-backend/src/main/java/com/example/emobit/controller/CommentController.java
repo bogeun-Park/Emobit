978066ab30337e7435461f078260c09cfed0dbfb
pbg <qhrms0528@softbase.co.kr>
2025-05-08 14:10:29 +0900

    댓글조회, 생성 api추가

---
+++ Emobit-backend/src/main/java/com/example/emobit/controller/CommentController.java
@@ -0,0 +1,47 @@
+package com.example.emobit.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.emobit.domain.Comments;
+import com.example.emobit.dto.CommentsCreateDto;
+import com.example.emobit.security.CustomUser;
+import com.example.emobit.service.CommentsService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class CommentController {
+	private final CommentsService commentsService;
+	
+	@GetMapping("/comments/{boardId}")
+	public ResponseEntity<?> getComment(@PathVariable("boardId") Long boardId) {
+		List<Comments> comments = commentsService.findAllByBoardId(boardId);
+		
+		return ResponseEntity.ok(comments);
+	}
+	
+	@PostMapping("/comments/create_process")
+	public ResponseEntity<?> boardCreateProcess(@RequestBody @Valid CommentsCreateDto commentsCreateDto,
+												@AuthenticationPrincipal CustomUser customUser) {
+		if (customUser == null) {
+	        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인이 필요합니다.");
+	    }
+		
+		commentsService.createComments(customUser.getId(), commentsCreateDto);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body("댓글 작성 성공");
+	}
+}
