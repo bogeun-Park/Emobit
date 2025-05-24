83dc466f605312e1dc69b38a9d81a1cb51206b22
bogeun-Park <qhrms0528@gmail.com>
2025-05-24 23:31:13 +0900

    댓글 전용 DTO 생성

---
+++ Emobit-backend/src/main/java/com/example/emobit/dto/CommentsDto.java
@@ -0,0 +1,34 @@
+package com.example.emobit.dto;
+
+import java.util.Date;
+
+import com.example.emobit.domain.Comments;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class CommentsDto {
+	private Long id;
+	private String content;
+	private Long boardId;
+	private Date createdAt;
+	private Date updatedAt;
+	private Long memberId;
+	private String memberUsername;
+	private String memberDisplayName;
+	
+	public CommentsDto(Comments comment) {
+		this.id = comment.getId();
+		this.content = comment.getContent();
+		this.boardId = comment.getBoard().getId();
+		this.createdAt = comment.getCreatedAt();
+		this.updatedAt = comment.getUpdatedAt();
+		this.memberId = comment.getMember().getId();
+		this.memberUsername = comment.getMember().getUsername();
+		this.memberDisplayName = comment.getMember().getDisplayName();
+	}
+}
