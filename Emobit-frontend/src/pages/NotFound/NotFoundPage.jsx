2562cb6bc49c51bccdef19f5a8847f6f01df157c
bogeun-Park <qhrms0528@gmail.com>
2025-05-24 19:05:10 +0900

    존재하지 않는 페이지 진입시 menu의 상테를 공백으로 세팅

--- Emobit-frontend/src/pages/NotFound/NotFoundPage.jsx
+++ Emobit-frontend/src/pages/NotFound/NotFoundPage.jsx
@@ -1,4 +1,15 @@
+import React, { useEffect } from 'react';
+import { menuAction } from '../../redux/Slice/menuSlice';
+import { useDispatch, useSelector } from 'react-redux';
+
 function NotFoundPage() {
+    const dispatch = useDispatch();
+    const active = useSelector(state => state.menu.active);
+
+    useEffect(() => {
+        dispatch(menuAction.setActiveMenu(''));
+    }, [active, dispatch]);
+
     return (
         <div>
             <h3>존재하지 않는 페이지입니다.</h3>
