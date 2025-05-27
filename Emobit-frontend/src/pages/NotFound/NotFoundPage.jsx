0c034ae9a1e82e6b30fa0d1fd25fcb26ae038960
pbg <qhrms0528@softbase.co.kr>
2025-05-27 17:49:48 +0900

    NotFound 페이지 디자인 변경

--- Emobit-frontend/src/pages/NotFound/NotFoundPage.jsx
+++ Emobit-frontend/src/pages/NotFound/NotFoundPage.jsx
@@ -1,6 +1,9 @@
+import '../../styles/NotFoundPage.css';
 import React, { useEffect } from 'react';
 import { menuAction } from '../../redux/Slice/menuSlice';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { CircleAlert } from 'lucide-react';
 
 function NotFoundPage() {
     const dispatch = useDispatch();
@@ -11,8 +14,17 @@ function NotFoundPage() {
     }, [active, dispatch]);
 
     return (
-        <div>
-            <h3>존재하지 않는 페이지입니다.</h3>
+        <div className="notfound-container">
+            <div className="notfound-box">
+                <div className="notfound-icon-wrapper">
+                    <CircleAlert className="notfound-icon" size={26}/>
+                </div>
+                
+                <div className="notfound-text1">이 페이지는 존재하지 않습니다.</div>
+                <div className="notfound-text2">링크가 잘못되었거나 프로필이 삭제되었을 수 있습니다.</div>
+                
+                <Link to="/" className="notfound-button">홈으로 돌아가기</Link>
+            </div>
         </div>
     );
 }
