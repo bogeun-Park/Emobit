599df9d7015fb9182bd5d8cee8006f39d97512e4
pbg <qhrms0528@softbase.co.kr>
2025-05-28 17:03:55 +0900

    사용자 디폴트 이미지 url 공통 상수 선언

--- Emobit-backend/src/main/java/com/example/emobit/util/Constant.java
+++ Emobit-backend/src/main/java/com/example/emobit/util/Constant.java
@@ -2,4 +2,5 @@ package com.example.emobit.util;
 
 public class Constant {
 	 public static final String BOARD_DEFAULT_IMG_URL = "https://objectstorage.ap-chuncheon-1.oraclecloud.com/n/axsd3bml0uow/b/EmobitBucket/o/common%2Fboard_defaultImage.png";
+	 public static final String MEMBER_DEFAULT_IMG_URL = "https://objectstorage.ap-chuncheon-1.oraclecloud.com/n/axsd3bml0uow/b/EmobitBucket/o/common%2Fmember_defaultImage.png";
 }
