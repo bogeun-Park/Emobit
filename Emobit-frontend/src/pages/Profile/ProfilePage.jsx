b867471966b4a71cf1a40755480f5eb1aeec80c2
pbg <qhrms0528@softbase.co.kr>
2025-05-29 13:04:17 +0900

    url의 username파라미터에 따라서 프로필을 나타내도록 수정, 프로필이 없는 경우 NotFoundPage 추가

--- Emobit-frontend/src/pages/Profile/ProfilePage.jsx
+++ Emobit-frontend/src/pages/Profile/ProfilePage.jsx
@@ -1,44 +1,54 @@
 import '../../styles/ProfilePage.css';
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
 import { useAxios } from '../../contexts/AxiosContext';
+import NotFoundPage from '../NotFound/NotFoundPage';
 
 function ProfilePage() {
     const axios = useAxios();
-    const auth = useSelector(state => state.auth);
 
-    const [myBoards, setMyBoards] = useState([]);
+    const { username } = useParams();
+    const [member, setMember] = useState(null);
+    const [boards, setBoards] = useState(null);
+    const [notFound, setNotFound] = useState(false);
 
     useEffect(() => {
-        if (!auth.isAuthenticated) {
-            return;
-        }
-
-        axios.get(`/myBoards/${auth.id}`)
+        axios.get(`/profile/${username}`)
             .then((response) => {
-                setMyBoards(response.data);
+                setMember(response.data.member);
+                setBoards(response.data.boards);
+                setNotFound(false);              
             })
-            .catch((err) => {
-                console.error('게시판 목록 불러오기 실패:', err);
+            .catch((error) => {
+                if (error.response && error.response.status === 404) {
+                    setNotFound(true);
+                } else {
+                    console.error('프로필 불러오기 실패:', error);
+                }
             });
-    }, [auth]);
+    }, [username]);
+
+    if (notFound) {
+        return <NotFoundPage />;
+    }
+
+    if (!boards || !member) return null;
 
     return (
         <div className="profile-container">
             <div className="profile-header">
-                <img src={auth.imageUrl} alt="" className="profile-image" />
+                <img src={member.imageUrl} alt="" className="profile-image" />
                 <div className="profile-info">
-                    <h2>{auth.displayName}</h2>
-                    <p>@{auth.username}</p>
-                    <p>게시물 {myBoards.length}</p>
+                    <h2>{member.displayName}</h2>
+                    <p>@{member.username}</p>
+                    <p>게시물 {boards.length}</p>
                 </div>
             </div>
 
             <hr className="profile-divider" />
 
             <div className="post-grid">
-                {myBoards.map((board) => (
+                {boards.map((board) => (
                     <Link to={`/board/read/${board.id}`} key={board.id} className="post-item">
                         <img src={board.imageUrl} alt={board.title}/>
                     </Link>
