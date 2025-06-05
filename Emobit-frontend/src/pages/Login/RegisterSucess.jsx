1f373940b490e548522ea09ccdb2a6e5c93f5270
pbg <qhrms0528@softbase.co.kr>
2025-06-05 14:07:44 +0900

    회원가입 성공 페이지 디자인 변경

--- Emobit-frontend/src/pages/Login/RegisterSucess.jsx
+++ Emobit-frontend/src/pages/Login/RegisterSucess.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import '../../styles/RegisterSucess.css';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function RegisterSucess() {
+function RegisterSuccess() {
+    const navigate = useNavigate();
+
     return (
-        <div>
-            <h2>가입되었습니다.</h2>
+        <div className="register-success-container">
+            <h2>가입되었습니다!</h2>
+            <p>환영합니다! 이제 로그인 페이지로 이동할 수 있습니다.</p>
+            <button onClick={() => navigate('/login')} className="login-redirect-button">
+                로그인하러 가기
+            </button>
         </div>
     );
 }
 
-export default RegisterSucess;
+export default RegisterSuccess;
