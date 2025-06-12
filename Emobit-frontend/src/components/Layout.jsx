5bd89e831c6a1be771181a25d02ea363a00754af
pbg <qhrms0528@softbase.co.kr>
2025-06-12 14:44:26 +0900

    소스 수정

--- Emobit-frontend/src/components/Layout.jsx
+++ Emobit-frontend/src/components/Layout.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
 
-function Layout({ children }) {
+function Layout() {
     return (
-        <div className="layout">
+        <div className="layout-container">
             <Sidebar />
             <main className="main-content">
                 <Outlet />
