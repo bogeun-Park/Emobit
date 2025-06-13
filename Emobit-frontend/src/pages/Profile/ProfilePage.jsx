6e20ddeb4c05e492a01b27a2cfc26c6f22d6e4a8
pbg <qhrms0528@softbase.co.kr>
2025-06-13 11:18:47 +0900

    사용자 이미지를 변경하는 기능 추가

--- Emobit-frontend/src/pages/Profile/ProfilePage.jsx
+++ Emobit-frontend/src/pages/Profile/ProfilePage.jsx
@@ -1,11 +1,15 @@
 import '../../styles/ProfilePage.css';
 import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { useAxios } from '../../contexts/AxiosContext';
 import NotFoundPage from '../NotFound/NotFoundPage';
+import presignedUrlAxios from 'axios';
 
 function ProfilePage() {
     const axios = useAxios();
+    const auth = useSelector(state => state.auth);
+    const navigate = useNavigate();
 
     const { username } = useParams();
     const [member, setMember] = useState(null);
@@ -28,6 +32,46 @@ function ProfilePage() {
             });
     }, [username]);
 
+    const handleClickImage = () => {
+        if (auth.username === username) {
+            document.getElementById('fileInput').click()
+        }
+    };
+
+    const handleImageChange = async (e) => {
+        const image = e.target.files[0];
+
+        if (!image) return;
+
+        let imageUrl = '';
+        try {
+            if (image) {
+                // Presigned URL 요청
+                const presignedRes = await axios.get(`/member/PresignedUrl?filename=${image.name}`);
+                const presignedUrl = presignedRes.data;
+
+                // Object Storage에 이미지 업로드
+                await presignedUrlAxios.put(presignedUrl, image, {
+                    headers: { 'Content-Type': image.type },
+                });
+
+                imageUrl = presignedUrl.replace(/\/p\/[^/]+\//, "/");
+
+                await axios.put(`/member/imageUrl_update/${auth.id}?imageUrl=${encodeURIComponent(imageUrl)}`)
+
+                setMember(prev => ({ ...prev, imageUrl: imageUrl }));
+            }
+        } catch (error) {
+            console.error('에러 발생:', error);
+            if (error.response?.status === 401) {
+                alert('로그인 세션이 만료되었습니다. 다시 로그인해주세요.');
+                navigate('/login');
+            } else {
+                alert('프로필 사진 변경중 오류가 발생했습니다.');
+            }
+        }
+    };
+
     if (notFound) {
         return <NotFoundPage />;
     }
@@ -37,11 +81,18 @@ function ProfilePage() {
     return (
         <div className="profile-container">
             <div className="profile-header">
-                <img src={member.imageUrl} alt="" className="profile-image" />
+                <img className={`profile-image ${auth.username === member.username ? 'clickable' : ''}`}
+                    src={member.imageUrl} alt="" onClick={handleClickImage}
+                />
+                <input type="file" id="fileInput" accept="image/*" style={{ display: 'none' }}
+                    onChange={handleImageChange}
+                    onClick={(e) => { e.target.value = null; }}
+                />
+
                 <div className="profile-info">
                     <p className="profile-info-username">{member.username}</p>
                     <p className="profile-info-stats">
-                        <div>게시물 <span className='profile-info-board-count'>{boards.length}</span></div>
+                        게시물 <span className='profile-info-board-count'>{boards.length}</span>
                     </p>
                     <p className="profile-info-displayName">{member.displayName}</p>
                 </div>
