a1025bd8378c872768665c0a41a4dee80cce0086
pbg <qhrms0528@softbase.co.kr>
2025-06-17 18:22:07 +0900

    사용자 채팅방 참여 여부 컬럼 추가, 채팅 메시지와 OneToMany 관계 추가

--- Emobit-backend/src/main/java/com/example/emobit/domain/ChatRoom.java
+++ Emobit-backend/src/main/java/com/example/emobit/domain/ChatRoom.java
@@ -1,14 +1,20 @@
 package com.example.emobit.domain;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.SequenceGenerator;
 import lombok.Getter;
 import lombok.Setter;
@@ -31,7 +37,18 @@ public class ChatRoom {
 	@Column(name = "USER_B", nullable = false)
 	private String userB;
 	
+	@Column(name = "USER_A_JOINED", nullable = false)
+	private boolean userAJoined = false;
+
+	@Column(name = "USER_B_JOINED", nullable = false)
+	private boolean userBJoined = false;
+	
 	@CreationTimestamp
 	@Column(name = "CREATED_AT", nullable = false, updatable = false)
     private Date createdAt;
+	
+	@ToString.Exclude
+	@OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL, orphanRemoval = true)
+	@JsonBackReference
+	private List<ChatMessage> chatMessages = new ArrayList<>();
 }
