2eb435fab76259da80bfbe448cced462492f9d2b
pbg <qhrms0528@softbase.co.kr>
2025-06-20 10:16:34 +0900

    환경변수로 프론트엔드 Origin 주소를 설정하는 방식으로 수정

--- Emobit-backend/src/main/java/com/example/emobit/config/WebConfig.java
+++ Emobit-backend/src/main/java/com/example/emobit/config/WebConfig.java
@@ -1,14 +1,18 @@
 package com.example.emobit.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+	@Value("${url.host.emobit}")
+    private String allowedOrigin;
+	
 	public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(allowedOrigin)
                 .allowedMethods("*")
         		.allowCredentials(true);
     }
