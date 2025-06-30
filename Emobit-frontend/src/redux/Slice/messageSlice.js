3cb229301bf4842331870eed29af2b6dfacc4136
pbg <qhrms0528@softbase.co.kr>
2025-06-30 16:45:39 +0900

    전역으로 메시지 상태를 관리하는 Slice 추가

---
+++ Emobit-frontend/src/redux/Slice/messageSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const messageSlice = createSlice({
+    name: 'message',
+    initialState: {
+        totalUnreadCount: 0,
+        chatRooms: [],
+    },
+    reducers: {
+        setChatRooms: (state, action) => {
+            state.chatRooms = action.payload;
+            state.totalUnreadCount = action.payload.reduce((sum, chatRoom) => sum + (chatRoom.unreadCount || 0), 0);
+        },
+        setTotalUnreadCount: (state, action) => {
+            state.totalUnreadCount = action.payload;
+        },
+        exitChatRoom: (state, action) => {
+            state.chatRooms = state.chatRooms.filter(chatRoom => chatRoom.id !== action.payload);
+        },
+        addChatRoom: (state, action) => {
+            const exists = state.chatRooms.some(chatRoom => chatRoom.id === action.payload.id);
+            if (!exists) state.chatRooms.push(action.payload);
+        },
+    }
+});
+
+export const messageAction = messageSlice.actions;
+export default messageSlice.reducer;
--- Emobit-frontend/src/redux/store.js
+++ Emobit-frontend/src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './Slice/authSlice';
 import menuReducer from './Slice/menuSlice';
 import searchReducer from './Slice/searchSlice';
+import messageReducer from './Slice/messageSlice';
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         menu: menuReducer,
         search: searchReducer,
+        message: messageReducer,
     }
 });
