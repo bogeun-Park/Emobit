00bf0d83acc0a86ecef9a52507cbd0c8caa8a00f
pbg <qhrms0528@softbase.co.kr>
2025-07-02 12:18:22 +0900

    댓글 개수를 저장하는 상태 추가

--- Emobit-frontend/src/redux/Slice/notificationSlice.js
+++ Emobit-frontend/src/redux/Slice/notificationSlice.js
@@ -3,11 +3,13 @@ import { createSlice } from '@reduxjs/toolkit';
 const notificationSlice = createSlice({
     name: 'notification',
     initialState: {
+        commentCount: 0,
         notifications: [],
     },
     reducers: {
         setNotifications: (state, action) => {
             state.notifications = action.payload;
+            state.commentCount = action.payload.filter(notification => notification.type == "COMMENT" && !notification.read).length;
         },
     },
 });
