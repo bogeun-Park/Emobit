e38267fe465c67414af91ebd82b24461987a056a
pbg <qhrms0528@softbase.co.kr>
2025-07-04 17:09:41 +0900

    읽지 않은 상태의 알림을 가져오는 메서드 추가

--- Emobit-backend/src/main/java/com/example/emobit/repository/NotificationRepository.java
+++ Emobit-backend/src/main/java/com/example/emobit/repository/NotificationRepository.java
@@ -1,16 +1,24 @@
 package com.example.emobit.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.emobit.domain.Member;
 import com.example.emobit.domain.Notification;
+import com.example.emobit.enums.NotificationType;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 	List<Notification> findByReceiverOrderByCreatedAtDesc(Member receiver);
 
 	List<Notification> findByReceiverAndIsReadFalse(Member receiver);
+	
+	// 읽지 않은 상태의 알림을 가져옴
+	Optional<Notification> findByReceiverAndSenderAndTypeAndTargetIdAndIsReadFalse(Member receiver, 
+																				   Member sender, 
+																				   NotificationType type, 
+																				   Long targetId);
 }
