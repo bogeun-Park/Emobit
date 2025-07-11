ba0bc45b25908cac91f8f48425b8eb02ddd08c1f
pbg <qhrms0528@softbase.co.kr>
2025-07-11 16:21:11 +0900

    회원가입 페이지 문구 변경

--- Emobit-frontend/src/pages/Login/RegisterSucess.jsx
+++ Emobit-frontend/src/pages/Login/RegisterSucess.jsx
@@ -8,7 +8,7 @@ function RegisterSuccess() {
     return (
         <div className="register-success-container">
             <h2>가입되었습니다!</h2>
-            <p>환영합니다! 이제 로그인 페이지로 이동할 수 있습니다.</p>
+            <p>환영합니다! 로그인 후 다양한 서비스를 이용해 보세요.</p>
             <button onClick={() => navigate('/login')} className="login-redirect-button">
                 로그인하러 가기
             </button>
