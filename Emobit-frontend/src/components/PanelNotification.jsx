5d1c3653edeb896fa0414d7745eca99ae788c3df
pbg <qhrms0528@softbase.co.kr>
2025-07-22 11:10:56 +0900

    알림 목록을 전체 삭제하는 기능 구현

--- Emobit-frontend/src/components/PanelNotification.jsx
+++ Emobit-frontend/src/components/PanelNotification.jsx
@@ -1,11 +1,14 @@
 import '../styles/PanelNotification.css';
 import React, { useEffect, useRef } from 'react';
+import { useAxios } from '../contexts/AxiosContext';
 import { useDispatch, useSelector } from 'react-redux';
 import { menuAction } from '../redux/Slice/menuSlice';
 import { useNavigate } from 'react-router-dom';
+import { notificationAction } from '../redux/Slice/notificationSlice';
 import { Heart } from 'lucide-react';
 
 function PanelNotification() {
+    const axios = useAxios();
     const panelMenu = useSelector(state => state.menu.panelMenu);
     const notifications = useSelector(state => state.notification.notifications);
     const panelRef = useRef(null);
@@ -46,9 +49,31 @@ function PanelNotification() {
         }
     };
 
+    const handleDeleteAll = () => {
+        const confirmed = window.confirm('알림을 모두 삭제하시겠습니까?');
+        if (!confirmed) return;
+
+        axios.delete('/notification/delete_all_process')
+            .then(() => {
+                dispatch(notificationAction.clearNotifications());
+            })
+            .catch((error) => {
+                console.error('에러 발생:', error);
+                if (error.response && error.response.status === 401) {
+                    alert('로그인 세션이 만료되었습니다. 다시 로그인해주세요.');
+                    navigate('/login');
+                } else {
+                    alert('알림 삭제 중 오류가 발생했습니다.');
+                }
+            });
+    }
+
     return (
         <div className={`panel-notification-container ${panelMenu === 'notification' ? 'open' : ''}`} ref={panelRef}>
-            <div className="notification-header">알림</div>
+            <div className="notification-header">
+                <span>알림</span>
+                {notifications.length > 0 && <button onClick={handleDeleteAll}>모두삭제</button>}
+            </div>
 
             <div className="notification-list">
                 {notifications.length === 0 ? (
