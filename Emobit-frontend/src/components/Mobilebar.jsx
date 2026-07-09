9a85a5331ae38c8dd9855fc716684d2f25c7f141
pbg <qhrms0528@softbase.co.kr>
2026-07-09 16:13:20 +0900

    모바일 상단바(topbar)에 로그인/로그아웃 버튼 추가

--- Emobit-frontend/src/components/Mobilebar.jsx
+++ Emobit-frontend/src/components/Mobilebar.jsx
@@ -2,13 +2,17 @@ import '../styles/Mobilebar.css'
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { useAxios } from '../contexts/AxiosContext';
+import { authAction } from '../redux/Slice/authSlice';
 import { menuAction } from '../redux/Slice/menuSlice';
 import { searchAction } from '../redux/Slice/searchSlice';
-import { Home, Search, BookOpen } from 'lucide-react';
+import { Home, Search, BookOpen, LogIn, LogOut } from 'lucide-react';
 
 function Mobilebar() {
+    const axios = useAxios();
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    const auth = useSelector(state => state.auth);
     const active = useSelector(state => state.menu.active);
     const panelMenu = useSelector(state => state.menu.panelMenu);
 
@@ -36,12 +40,40 @@ function Mobilebar() {
         dispatch(menuAction.setPanelMenu(menuName));
     };
 
+    const handlelogout = () => {
+        axios.post('/logout')
+            .then(response => {
+                console.log(response);
+
+                dispatch(authAction.logout());
+
+                setTimeout(() => {
+                    dispatch(searchAction.clearSearchState())
+                }, 300);
+
+                navigate('/login');
+            })
+            .catch(error => {
+                console.error('Failed to fetch user data:', error);
+            });
+    };
+
     return (
         <div className="mobilebar-container">
             <header className="mobile-topbar">
                 <Link to="/" className="mobile-logo" onClick={() => handleMenuClick('/')}>
                     Emobit
                 </Link>
+
+                {!auth.isAuthenticated ? (
+                    <button className="mobile-auth-button" onClick={() => handleMenuClick('/login')}>
+                        <LogIn size={22} />
+                    </button>
+                ) : (
+                    <button className="mobile-auth-button" onClick={handlelogout}>
+                        <LogOut size={22} />
+                    </button>
+                )}
             </header>
 
             <nav className="mobile-bottombar">
